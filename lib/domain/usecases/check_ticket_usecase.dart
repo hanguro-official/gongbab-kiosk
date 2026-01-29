af2eef22fc35344077b9d97a9fc96f487ca9798c
devdbsdngml <wh9614y@gmail.com>
2026-01-29 13:01:22 +0900

    refactor(domain): remove injectable dependency from use cases
    Removes the 'injectable' annotation and the corresponding package import from CheckTicketUseCase and GetKioskStatusUseCase.
    
    This change makes the domain layer's use cases independent of the dependency injection framework, adhering to pure Dart principles for this layer.

--- lib/domain/usecases/check_ticket_usecase.dart
+++ lib/domain/usecases/check_ticket_usecase.dart
@@ -1,7 +1,5 @@
 import 'package:gongbab/domain/repositories/kiosk_repository.dart';
-import 'package:injectable/injectable.dart'; // injectable 임포트
 
-@injectable // 등록
 class CheckTicketUseCase {
   final KioskRepository repository;
 
--- lib/domain/usecases/get_kiosk_status_usecase.dart
+++ lib/domain/usecases/get_kiosk_status_usecase.dart
@@ -1,8 +1,6 @@
 import 'package:gongbab/domain/entities/kiosk_status.dart';
 import 'package:gongbab/domain/repositories/kiosk_repository.dart';
-import 'package:injectable/injectable.dart'; // injectable 임포트
 
-@injectable // 등록
 class GetKioskStatusUseCase {
   final KioskRepository repository;
 
