0866a83c4464a8f7860ae8504c7d926e8ce88c48
devdbsdngml <wh9614y@gmail.com>
2026-02-05 23:49:42 +0900

    fix: error di initialize

--- lib/di/injection.config.dart
+++ lib/di/injection.config.dart
@@ -18,6 +18,7 @@ import '../data/network/app_api_client.dart' as _i133;
 import '../data/repositories/kiosk_repository_impl.dart' as _i400;
 import '../domain/repositories/kiosk_repository.dart' as _i587;
 import '../domain/usecases/get_kiosk_status_usecase.dart' as _i5;
+import 'injection.dart' as _i464;
 
 extension GetItInjectableX on _i174.GetIt {
 // initializes the registration of main-scope dependencies inside of GetIt
@@ -30,13 +31,18 @@ extension GetItInjectableX on _i174.GetIt {
       environment,
       environmentFilter,
     );
+    final registerModule = _$RegisterModule();
     gh.singleton<_i133.AppApiClient>(() => _i133.AppApiClient());
     gh.singleton<_i589.ApiService>(
         () => _i589.ApiService(gh<_i133.AppApiClient>()));
-    gh.factory<_i513.PhoneNumberInputViewModel>(
-        () => _i513.PhoneNumberInputViewModel(gh<_i5.GetKioskStatusUseCase>()));
     gh.lazySingleton<_i587.KioskRepository>(
         () => _i400.KioskRepositoryImpl(gh<_i589.ApiService>()));
+    gh.lazySingleton<_i5.GetKioskStatusUseCase>(() =>
+        registerModule.getKioskStatusUseCase(gh<_i587.KioskRepository>()));
+    gh.factory<_i513.PhoneNumberInputViewModel>(
+        () => _i513.PhoneNumberInputViewModel(gh<_i5.GetKioskStatusUseCase>()));
     return this;
   }
 }
+
+class _$RegisterModule extends _i464.RegisterModule {}
--- lib/di/injection.dart
+++ lib/di/injection.dart
@@ -1,6 +1,5 @@
 import 'package:get_it/get_it.dart';
 import 'package:injectable/injectable.dart';
-import '../domain/usecases/get_employee_candidates_usecase.dart';
 import 'injection.config.dart'; // 코드 생성기가 생성할 파일
 import 'package:gongbab/domain/repositories/kiosk_repository.dart';
 import 'package:gongbab/domain/usecases/get_kiosk_status_usecase.dart';
@@ -21,10 +20,5 @@ abstract class RegisterModule {
   @lazySingleton // Use lazySingleton for use cases
   GetKioskStatusUseCase getKioskStatusUseCase(KioskRepository repository) {
     return GetKioskStatusUseCase(repository);
-  }
-
-  @lazySingleton
-  GetEmployeeCandidatesUseCase getEmployeeCandidatesUseCase(KioskRepository repository) {
-    return GetEmployeeCandidatesUseCase(repository);
   }
 }
