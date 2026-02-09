50ea9e213f849b1b61f36fb5c38e44cb6f01e675
devdbsdngml <wh9614y@gmail.com>
2026-02-09 12:17:45 +0900

    feat: fix di config

--- lib/di/injection.config.dart
+++ lib/di/injection.config.dart
@@ -8,6 +8,7 @@
 // coverage:ignore-file
 
 // ignore_for_file: no_leading_underscores_for_library_prefixes
+import 'package:connectivity_plus/connectivity_plus.dart' as _i895;
 import 'package:get_it/get_it.dart' as _i174;
 import 'package:injectable/injectable.dart' as _i526;
 
@@ -35,6 +36,7 @@ extension GetItInjectableX on _i174.GetIt {
     );
     final registerModule = _$RegisterModule();
     gh.singleton<_i133.AppApiClient>(() => _i133.AppApiClient());
+    gh.lazySingleton<_i895.Connectivity>(() => registerModule.connectivity);
     gh.singleton<_i589.ApiService>(
         () => _i589.ApiService(gh<_i133.AppApiClient>()));
     gh.lazySingleton<_i587.KioskRepository>(
