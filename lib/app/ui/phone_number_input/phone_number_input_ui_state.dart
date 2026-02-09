c9eb8441cc8b5343bb97485104727fa8175561dd
devdbsdngml <wh9614y@gmail.com>
2026-02-09 10:20:29 +0900

    feat(phone_input): Define UI states for phone number input flow

--- lib/app/ui/phone_number_input/phone_number_input_ui_state.dart
+++ lib/app/ui/phone_number_input/phone_number_input_ui_state.dart
@@ -1,20 +1,34 @@
+import 'package:gongbab/domain/entities/check_in/kiosk_check_in.dart';
+import 'package:gongbab/domain/entities/lookup/employee_match.dart';
 import 'package:gongbab/domain/entities/status/kiosk_status.dart';
 
 // UI State
 sealed class PhoneNumberInputUiState {}
 
-class PhoneNumberInputInitial extends PhoneNumberInputUiState {}
+class Initial extends PhoneNumberInputUiState {}
 
-class PhoneNumberInputLoading extends PhoneNumberInputUiState {}
+class Loading extends PhoneNumberInputUiState {}
 
-class PhoneNumberInputSuccess extends PhoneNumberInputUiState {
+class KioskStatusLoaded extends PhoneNumberInputUiState {
   final KioskStatus kioskStatus;
 
-  PhoneNumberInputSuccess(this.kioskStatus);
+  KioskStatusLoaded(this.kioskStatus);
 }
 
-class PhoneNumberInputError extends PhoneNumberInputUiState {
+class EmployeeCandidatesLoaded extends PhoneNumberInputUiState {
+  final List<EmployeeMatch> employees;
+
+  EmployeeCandidatesLoaded(this.employees);
+}
+
+class CheckInSuccess extends PhoneNumberInputUiState {
+  final KioskCheckIn checkInResult;
+
+  CheckInSuccess(this.checkInResult);
+}
+
+class Error extends PhoneNumberInputUiState {
   final String message;
 
-  PhoneNumberInputError(this.message);
+  Error(this.message);
 }
