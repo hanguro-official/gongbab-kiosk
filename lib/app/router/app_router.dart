ddf8f47fa32e0f98fca3ee5dad261ac835298d6a
devdbsdngml <wh9614y@gmail.com>
2026-02-12 02:23:41 +0900

    etc

--- lib/app/router/app_router.dart
+++ lib/app/router/app_router.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:gongbab/app/ui/login/login_screen.dart';
 import 'package:gongbab/app/ui/phone_number_input/phone_number_input_screen.dart';
@@ -35,25 +36,24 @@ class AppRouter {
           },
         ),
       ],
-      redirect: (BuildContext context, GoRouterState state) async {
-        final accessToken = await _authTokenManager.getAccessToken();
-        final refreshToken = await _authTokenManager.getRefreshToken();
-
-        final loggedIn = accessToken != null && refreshToken != null;
-        final goingToLogin = state.matchedLocation == AppRoutes.root;
-
-        // If not logged in and not going to login, redirect to login
-        if (!loggedIn && !goingToLogin) {
-          return AppRoutes.root;
-        }
-        // If logged in and going to login, redirect to phone number input
-        if (loggedIn && goingToLogin) {
-          return AppRoutes.phoneNumberInput;
-        }
-
-        // No redirect
-        return null;
-      },
+      redirect: (BuildContext context, GoRouterState state) => redirectLogic(context, state),
     );
+  }
+
+  @visibleForTesting // Make it visible for testing
+  String? redirectLogic(BuildContext context, GoRouterState state) {
+    final accessToken = _authTokenManager.getAccessToken();
+    final refreshToken = _authTokenManager.getRefreshToken();
+
+    final loggedIn = accessToken != null && refreshToken != null;
+    final goingToLogin = state.matchedLocation == AppRoutes.root;
+
+    if (!loggedIn && !goingToLogin) {
+      return AppRoutes.root;
+    }
+    if (loggedIn && goingToLogin) {
+      return AppRoutes.phoneNumberInput;
+    }
+    return null;
   }
 }
